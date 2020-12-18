1b7f75523f80df48b6f1d3a3aec87de041f09412
Karthik Somu <Karthik.Somu@corp.internal>
2020-12-18 12:46:49 +0000

    add fos

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const jsonServer = require('josn-server');
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.josn');
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
